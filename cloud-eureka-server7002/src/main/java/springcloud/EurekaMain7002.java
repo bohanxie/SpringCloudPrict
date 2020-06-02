8ac25ce29554d23691d23312c6e35b14ca619b68
17350 <bohan.xie@hand-china.com>
2020-06-02 23:16:19 +0800

    [ADD]增加Eureka节点，搭建Eureka集群

---
+++ cloud-eureka-server7002/src/main/java/springcloud/EurekaMain7002.java
@@ -0,0 +1,20 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xbh
+ * @version 1.0.0
+ * @ClassName EurekaMain7001.java
+ * @Description 注册中心启动类
+ * @createTime 2020年06月01日 23:16:00
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
