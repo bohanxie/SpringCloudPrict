71f29aff7d3bc7d00217acd910d75b9bfb4ba88e
17350 <bohan.xie@hand-china.com>
2020-06-02 22:51:16 +0800

    [UPDATE]将8001服务提供者注册到注册中心

--- cloud-provider-payment-8001/src/main/java/pri/prictice/springcloud/PaymentMain8001.java
+++ cloud-provider-payment-8001/src/main/java/pri/prictice/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package pri.prictice.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author xbh
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @createTime 2020年04月21日 20:40:00
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
